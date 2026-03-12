e0a79fea97ad417723c588b90dc85ff45f6d2d39
robertor4 <roberto@dreamone.nl>
2026-03-12 20:39:17 +0100

    feat: add LinkedIn Insight Tag for conversion tracking

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import Script from 'next/script';
 import { Geist, Montserrat, Merriweather, DM_Mono } from 'next/font/google';
 import "./globals.css";
 import { ThemeColor } from "@/components/ThemeColor";
@@ -65,6 +66,29 @@ export default function RootLayout({ children }: Props) {
       </head>
       <body className={geist.className}>
         {children}
+        <Script id="linkedin-partner-id" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "8803626";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </Script>
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }} alt="" src="https://px.ads.linkedin.com/collect/?pid=8803626&fmt=gif" />
+        </noscript>
       </body>
     </html>
   );
